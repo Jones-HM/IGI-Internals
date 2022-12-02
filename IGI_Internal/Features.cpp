d0660802d5d07f4c8d0f5a7f9fa73d2b6d035650
IGI-Research-Devs <93740747+IGI-Research-Devs@users.noreply.github.com>
2022-12-02 07:41:42 +0400

    Updated Features with Example
    There are shown 5 examples into _Features.cpp_ file.
    - Enable Debug mode.
    - Restart game.
    - Weapon pickup.
    - Frames setting
    - Humanplayer load.

--- IGI_Internal/Features.cpp
+++ IGI_Internal/Features.cpp
@@ -1,7 +1,6 @@
 #include "Features.hpp"
 
 void DllMainLoop() {
-	static bool model_bool = false;
 	g_menu_screen = READ_PTR(menu_screen_ptr);
 	g_game_level = LEVEL::GET();
 	if (g_curr_level != g_game_level) {
@@ -30,7 +29,7 @@ void DllMainLoop() {
 
 	else if (g_menu_screen == MENU_SCREEN_INGAME) {
 
-		//Ctrl-Menu Controls.
+		// Enable Debug mode.
 		if (GT_HotKeysPressed(VK_CONTROL, VK_F1)) {
 			DEBUG::INIT(GAME_FONT_BIG);
 			DEBUG::ENABLE(g_DbgEnabled);
@@ -40,30 +39,28 @@ void DllMainLoop() {
 			g_DbgEnabled = !g_DbgEnabled;
 		}
 
+		// Restart game.
 		else if (GT_HotKeysPressed(VK_CONTROL, VK_F2)) {
-			QTASK::UPDATE();
-			g_AutoMsgBox->Show("", 70);
-			LEVEL::LOAD();
-			g_AutoMsgBox->Show("", 70);
+			RestartLevel();
 		}
 
+		// Weapon pickup - AK47.
 		else if (GT_HotKeysPressed(VK_CONTROL, VK_F3)) {
 			try {
-				auto internal_data = InternalDataRead();
-				int weapon_id = std::stoi(internal_data);
+				int weapon_id = GAME_WEAPON::WEAPON_ID_AK47;
 				WEAPON::WEAPON_PICKUP(weapon_id);
+				MISC::STATUS_MESSAGE_SHOW(string("WeaponPickup Id:" + std::to_string(weapon_id) + " done!"));
 			}
 			catch (const std::exception& ex)
 			{
 				LOG_INFO("Exception: %s", ex.what());
 			}
-			//MISC::STATUS_MESSAGE_SHOW(string("WeaponPickup Id:" + std::to_string(weapon_id) + " done!"));
 		}
 
+		// Frames setting - 60 FPS.
 		else if (GT_HotKeysPressed(VK_CONTROL, VK_F4)) {
 			try {
-				auto internal_data = InternalDataRead();
-				int frames = std::stoi(internal_data);
+				int frames = 60;
 				MISC::FRAMES_SET(frames);
 				MISC::STATUS_MESSAGE_SHOW(string("Game frames changed to " + std::to_string(frames) + "FPS"));
 			}
@@ -73,434 +70,10 @@ void DllMainLoop() {
 			}
 		}
 
+		// Humanplayer load.
 		else if (GT_HotKeysPressed(VK_CONTROL, VK_F5)) {
-			CONFIG::READ();
-			MISC::STATUS_MESSAGE_SHOW("Game config read!");
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F6)) {
-			CONFIG::WRITE();
-			MISC::STATUS_MESSAGE_SHOW("Game config write!");
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F7)) {
-			CONFIG::WEAPON_CONFIG_READ();
-			MISC::STATUS_MESSAGE_SHOW("Weapon config read!");
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F8)) {
 			HUMAN::HUMAN_PLAYER_LOAD();
 			MISC::STATUS_MESSAGE_SHOW("Humanplayer load success!");
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F9)) {
-			auto internal_data = InternalDataRead();
-			int view = std::stoi(internal_data);
-			int cam_view = (view <= 0 || view > 5) ? 1 : view;
-			HUMAN::CAM_VIEW_SET(cam_view);
-			MISC::STATUS_MESSAGE_SHOW("Humanplayer Camera #" + std::to_string(cam_view));
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F10)) {
-			GAME::INPUT_ENABLE();
-			g_PlayerEnabled = true;
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F11)) {
-			GAME::INPUT_DISABLE();
-			g_PlayerEnabled = false;
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_F12)) {
-			GAME::INPUT_DISABLE();
-			g_PlayerEnabled = false;
-
-			Camera::Controls controls;
-			controls.UP(VK_SPACE);
-			controls.DOWN(VK_MENU);
-			controls.LEFT(VK_LEFT);
-			controls.RIGHT(VK_RIGHT);
-			controls.FORWARD(VK_UP);
-			controls.BACKWARD(VK_DOWN);
-			controls.CALIBRATE(VK_BACK);
-			controls.QUIT(VK_HOME);
-			controls.AXIS_OFF(0.5f);
-
-			g_Camera.RunFreeCamThread(controls);
-			GAME::INPUT_ENABLE();
-			g_PlayerEnabled = true;
-		}
-
-		//Alt-Menu Controls.
-		else if (GT_HotKeysPressed(VK_MENU, VK_F1)) {
-			try {
-				string level = InternalDataRead();
-				LOG_INFO("StarLevel level '%s'", level.c_str());
-				StartLevelMain(std::stoi(level));
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F2)) {
-			QuitLevelMain();
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F3)) {
-			try {
-				string script_file = InternalDataRead();
-				LOG_INFO("Script Parsing file '%s'", script_file.c_str());
-				int status = SCRIPT::PARSE(script_file); //Status '0' success, 'Non-zero' error.
-				if (status == 0) MISC::STATUS_MESSAGE_SHOW("Script Parsing file done!");
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F4)) {
-			try {
-				string script_file = InternalDataRead();
-				LOG_INFO("Script Assembling file '%s'", script_file.c_str());
-				int status = SCRIPT::ASSEMBLE(script_file); //Status '0' success, 'Non-zero' error.
-				if (status == 0) MISC::STATUS_MESSAGE_SHOW("Script Assemble file done!");
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F5)) {
-			ScriptCompile();
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F6)) {
-			try {
-				string resource_file = InternalDataRead();
-				LOG_INFO("Resource Load file '%s'", resource_file.c_str());
-				auto resource_addr = RESOURCE::LOAD(resource_file);
-				string data = "Resource '" + resource_file + "' loaded at address " + HEX_ADDR_STR(resource_addr);
-				InternalDataWrite(data);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F7)) {
-			try {
-
-				string resource_file = InternalDataRead();
-				LOG_INFO("Resource Unload file '%s'", resource_file.c_str());
-				std::vector<string> res_list;
-				g_Utility.Tokenize(resource_file, '\n', res_list);
-				RESOURCE::UNLOAD(res_list);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F8)) {
-			try {
-				string resource_file = InternalDataRead();
-				LOG_INFO("Resource Unpack file '%s'", resource_file.c_str());
-				auto resource_addr = RESOURCE::UNPACK(resource_file);
-				string data = "Resource '" + resource_file + "' unpacked at address " + HEX_ADDR_STR(resource_addr);
-				InternalDataWrite(data);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F9)) {
-			try {
-				string resource_file = InternalDataRead();
-				LOG_INFO("Resource Flush file '%s'", resource_file.c_str());
-				auto resource_addr = RESOURCE::FIND(resource_file);
-				RESOURCE::FLUSH(resource_addr);
-				string data = "Resource '" + resource_file + "' found at address " + HEX_ADDR_STR(resource_addr);
-				InternalDataWrite(data);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F10)) {
-			try {
-				string resource_file = InternalDataRead(), data;
-				LOG_INFO("Resource is loaded file '%s'", resource_file.c_str());
-				bool is_loaded = RESOURCE::IS_LOADED(resource_file);
-				if (is_loaded) data = "TRUE"; else data = "FALSE";
-				InternalDataWrite(data);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F11)) {
-			try {
-				string resource_file = InternalDataRead();
-				LOG_INFO("Resource Unpack find name '%s'", resource_file.c_str());
-				auto resource_addr = RESOURCE::FIND(resource_file);
-				string data = "Resource '" + resource_file + "' found at address " + HEX_ADDR_STR(resource_addr);
-				LOG_INFO("'%s'", data.c_str());
-				InternalDataWrite(HEX_ADDR_STR(resource_addr));
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_MENU, VK_F12)) {
-			RESOURCE::ANIMATION_INFO_SAVE("IGI_Animations.txt");
-			RESOURCE::FONT_INFO_SAVE("IGI_Fonts.txt");
-			RESOURCE::SOUND_INFO_SAVE("IGI_Sound.txt");
-			RESOURCE::MATERIAL_INFO_SAVE("IGI_Material.txt");
-			RESOURCE::LIGHTMAP_INFO_SAVE("IGI_Lightmap.txt");
-			RESOURCE::OBJECT_INFO_SAVE("IGI_Object.txt");
-			RESOURCE::RESOURCE_INFO_SAVE("IGI_Resource.txt");
-			RESOURCE::TERRAIN_INFO_SAVE("IGI_Terrain.txt");
-			RESOURCE::TEXTURE_INFO_SAVE("IGI_Texture.txt");
-			RESOURCE::GRAPHICS_2D_INFO_SAVE("IGI_2D_Graphics.txt");
-			RESOURCE::GRAPHICS_3D_INFO_SAVE("IGI_3D_Graphics.txt");
-		}
-
-		//Shift-Menu Controls.
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F1)) {
-			try {
-				string model = InternalDataRead();
-				LOG_INFO("MEF Model remove '%s'", model.c_str());
-				RESOURCE::MEF_REMOVE_MODEL(model);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F2)) {
-			RESOURCE::MEF_RESTORE_MODELS();
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F3)) {
-			RESOURCE::MEF_EXTRACT();
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F4)) {
-			try {
-				string qvm_file = InternalDataRead();
-				LOG_INFO("QVM Load file '%s'", qvm_file.c_str());
-				auto qvm_addr = QVM::LOAD(qvm_file);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F5)) {
-			try {
-
-				string qvm_addr = InternalDataRead();
-				LOG_INFO("QVM Read Address '%s'", qvm_addr.c_str());
-				int status = QVM::READ(std::stoi(qvm_addr)); //Status '0' success, 'Non-zero' error.
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F6)) {
-
-			try {
-				string qvm_file = InternalDataRead();
-				LOG_INFO("QVM cleanup file '%s'", qvm_file.c_str());
-				auto qvm_addr = QVM::LOAD(qvm_file);
-				int status = QVM::READ((int)qvm_addr); //Overloaded method 'QVM::READ(int)'
-				if (status == 0) QVM::CLEANUP(qvm_addr);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F7)) {
-			SFX::MUSIC_ENABLE();
-			LOG_INFO("Music Enabled");
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F8)) {
-			SFX::MUSIC_DISABLE();
-			LOG_INFO("Music Disabled");
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F9)) {
-			string volume = InternalDataRead();
-			LOG_INFO("Volume set %s", volume.c_str());
-			SFX::VOLUME_SET(std::stof(volume));
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F10)) {
-			try {
-				string volume = InternalDataRead();
-				LOG_INFO("Volume SFX set %s", volume.c_str());
-				SFX::VOLUME_SFX_SET(std::stof(volume));
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F11)) {
-			try {
-				SFX::VOLUME_UPDATE();
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_SHIFT, VK_F12)) {
-			try {
-				GFX::RESET();
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		//Ctrl-Numpad Controls.
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_ADD)) {
-			try
-			{
-				string active_name = PLAYER::ACTIVE_NAME_GET();
-				LOG_INFO("Player active name %s", active_name.c_str());
-
-				string name(active_name.data());
-				InternalDataWrite(name);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_SUBTRACT)) {
-			try
-			{
-				int active_mission = PLAYER::ACTIVE_MISSION_GET();
-				LOG_INFO("Player active mission %d", active_mission);
-
-				if (active_mission > 0 && active_mission <= 14) {
-					InternalDataWrite(std::to_string(active_mission));
-				}
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD0)) {
-			string mission = InternalDataRead();
-			LOG_INFO("Player Active mission set %d", mission.c_str());
-			PLAYER::ACTIVE_MISSION_SET(std::stoi(mission));
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD1)) {
-			string name = InternalDataRead();
-			LOG_INFO("Player Active name set %d", name.c_str());
-			PLAYER::ACTIVE_NAME_SET(name);
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD2)) {
-			string data = InternalDataRead();
-			std::vector<string> vec;
-			g_Utility.Tokenize(data, '\n', vec);
-
-			int index = 0;
-			byte mission = 1;
-			index = std::stoi(vec.at(0));
-			mission = std::stoi(vec.at(1));
-
-			LOG_INFO("Player index mission set mission %d\tIndex: %d", mission, index);
-			PLAYER::INDEX_MISSION_SET(index, mission);
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD3)) {
-			string data = InternalDataRead();
-			std::vector<string> vec;
-			g_Utility.Tokenize(data, '\n', vec);
-
-			int index = 0;
-			string name = "Jones";
-			index = std::stoi(vec.at(0));
-			name = std::stoi(vec.at(1));
-
-			LOG_INFO("Player index name set name %s\tIndex: %d", name, index);
-			PLAYER::INDEX_NAME_SET(index, name);
-		}
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD4)) {
-			MISC::GAMEMATERIAL_LOAD();
-		}
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD5)) {
-			MISC::MAGIC_OBJECT_LOAD();
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD6)) {
-			MISC::PHYSICS_OBJECT_LOAD();
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD7)) {
-			MISC::ANIM_TRIGGER_LOAD();
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_NUMPAD8)) {
-			MISC::CUTSCENE_DELETE();
-			MISC::STATUS_MESSAGE_SHOW("Cutscene removed");
-		}
-
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_INSERT)) {
-			StatusMsgShow();
-		}
-
-		//Alt-Numpad Controls.
-		else if (GT_HotKeysPressed(VK_CONTROL, VK_HOME)) {
-			try
-			{
-				auto data = InternalDataRead();
-				int graph_id = std::stoi(data);
-				string node_links = GRAPH::GET_NODE_LINKS(graph_id);
-				InternalDataWrite(node_links);
-			}
-			catch (const std::exception& ex)
-			{
-				LOG_INFO("Exception: %s", ex.what());
-			}
-		}
-
-		else if (GT_IsKeyToggled(VK_SNAPSHOT)) {
-			g_Console->Clear();
 		}
 	}
 
@@ -511,54 +84,14 @@ void DllMainLoop() {
 
 }
 
-void StatusMsgShow() {
-	try {
-		string data = InternalDataRead();
-		MISC::STATUS_MESSAGE_SHOW(data);
-	}
-	catch (const std::exception& ex)
-	{
-		LOG_INFO("Exception: %s", ex.what());
-	}
-}
 
-void ScriptCompile() {
-	try {
-		string script_file = InternalDataRead();
-		SCRIPT::COMPILE(script_file);
-		LOG_INFO("Script Compile file '%s' done!", script_file.c_str());
-	}
-	catch (const std::exception& ex)
-	{
-		LOG_INFO("Exception: %s", ex.what());
-	}
-	//MISC::STATUS_MESSAGE_SHOW("Script Compile file done!");
-}
+#pragma region Native Helper Methods
 
-string InternalDataRead() {
-	string data;
-	try {
-		string internal_data_file = g_Utility.GetModuleFolder() + "\\" + PROJECT_NAME + "-data.txt";
-		std::ifstream in_stream(internal_data_file);
-
-		if (in_stream.good()) {
-			std::getline(in_stream, data);
-		}
-		else {
-			throw std::runtime_error("Internal data file doesn't exist in current directory");
-		}
-	}
-	catch (const std::exception& ex)
-	{
-		LOG_INFO("Exception: %s", ex.what());
-	}
-	return data;
-}
-
-bool InternalDataWrite(string data) {
-	string internal_data_file = g_Utility.GetModuleFolder() + "\\" + PROJECT_NAME + "-data.txt";
-	auto status = WriteFileType(internal_data_file, binary_t(data.begin(), data.end()), BINARY_FILE);
-	return status;
+void RestartLevel() {
+	QTASK::UPDATE();
+	g_AutoMsgBox->Show("", 70);
+	LEVEL::LOAD();
+	g_AutoMsgBox->Show("", 70);
 }
 
 void StartLevelMain(int level, bool disable_warn, bool disable_err, int hash_val) {
@@ -577,10 +110,10 @@ void StartLevelMain(int level, bool disable_warn, bool disable_err, int hash_val
 }
 
 void QuitLevelMain() {
-	auto sub_416d40 = (int(__cdecl*)())0x416d40;
+	auto sub_416d40 = (int(__cdecl*)())0x00416D40;
 	auto data = (int**)sub_416d40();
 
-	auto sub_004015f0 = (int(__cdecl*)(int**))0x004015f0;
+	auto sub_004015f0 = (int(__cdecl*)(int**))0x004015F0;
 	sub_004015f0(data);
 	*(int*)(0x00567c8c + 0x28) = 3; // Id of Main menu screen.
 
@@ -605,3 +138,4 @@ RT ThreadCallerExec(void* func_ptr, T1 param1, T2 param2, T3 param3, T4 param4) 
 	if constexpr (!std::is_same_v<RT, void>)
 		return ret_val;
 }
+#pragma endregion
